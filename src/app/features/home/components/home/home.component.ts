e43c742e0b58f73c78a2e73b7c1b012e4d92448a
arodoo <haro.em@hotmail.com>
2024-08-13 10:28:30 -0600

    navbar fixed, about added

--- src/app/features/home/components/home/home.component.ts
+++ src/app/features/home/components/home/home.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../../shared/components/header/header.component';
 import { FooterComponent } from '../../../../shared/components/footer/footer.component';
 import { NavbarComponent } from '../../../../shared/components/navbar/navbar.component';
+import { AboutComponent } from '../../../about/components/about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, NavbarComponent],
+  imports: [HeaderComponent, FooterComponent, NavbarComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
