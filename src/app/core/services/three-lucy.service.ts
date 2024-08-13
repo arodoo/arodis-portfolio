ba25d4cdfa841c8b3c4495f1e470de69058727ca
arodoo <haro.em@hotmail.com>
2024-08-13 10:13:15 -0600

    header working, body color dits on animations color

--- src/app/core/services/three-lucy.service.ts
+++ src/app/core/services/three-lucy.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, ElementRef, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import * as THREE from 'three';
-import {OrbitControls} from 'three-stdlib';
+import { OrbitControls } from 'three-stdlib';
 import { PLYLoader } from 'three-stdlib';
 
 @Injectable({
@@ -20,79 +20,101 @@ export class ThreeLucyService {
 
   init(canvas: ElementRef<HTMLCanvasElement>): void {
     if (isPlatformBrowser(this.platformId)) {
-      this.renderer = new THREE.WebGLRenderer({ canvas: canvas.nativeElement, antialias: true });
-      this.renderer.setPixelRatio(window.devicePixelRatio);
-      this.renderer.setSize(canvas.nativeElement.clientWidth, canvas.nativeElement.clientHeight);
-      this.renderer.shadowMap.enabled = true;
-      this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-      this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
-      this.renderer.toneMappingExposure = 1;
-
-      this.scene = new THREE.Scene();
-
-      this.camera = new THREE.PerspectiveCamera(40, canvas.nativeElement.clientWidth / canvas.nativeElement.clientHeight, 0.1, 100);
-      this.camera.position.set(7, 4, 1);
-
-      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-      this.controls.minDistance = 2;
-      this.controls.maxDistance = 10;
-      this.controls.maxPolarAngle = Math.PI / 2;
-      this.controls.target.set(0, 1, 0);
-      this.controls.update();
-
-      const ambient = new THREE.HemisphereLight(0xffffff, 0x8d8d8d, 0.15);
-      this.scene.add(ambient);
-
-      const loader = new THREE.TextureLoader().setPath('textures/');
-      const textures = {
-        'disturb.jpg': loader.load('disturb.jpg'),
-        'colors.png': loader.load('colors.png'),
-        'uv_grid_opengl.jpg': loader.load('uv_grid_opengl.jpg')
-      };
-
-      this.spotLight = new THREE.SpotLight(0xffffff, 100);
-      this.spotLight.position.set(2.5, 5, 2.5);
-      this.spotLight.angle = Math.PI / 6;
-      this.spotLight.penumbra = 1;
-      this.spotLight.decay = 2;
-      this.spotLight.distance = 0;
-      this.spotLight.map = textures['disturb.jpg'];
-      this.spotLight.castShadow = true;
-      this.spotLight.shadow.mapSize.width = 1024;
-      this.spotLight.shadow.mapSize.height = 1024;
-      this.spotLight.shadow.camera.near = 1;
-      this.spotLight.shadow.camera.far = 10;
-      this.spotLight.shadow.focus = 1;
-      this.scene.add(this.spotLight);
-
-      this.lightHelper = new THREE.SpotLightHelper(this.spotLight);
-      this.scene.add(this.lightHelper);
-
-      const planeGeometry = new THREE.PlaneGeometry(200, 200);
-      const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xbcbcbc });
-      const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-      plane.position.set(0, -1, 0);
-      plane.rotation.x = -Math.PI / 2;
-      plane.receiveShadow = true;
-      this.scene.add(plane);
-
-      new PLYLoader().load('models/ply/binary/Lucy100k.ply', (geometry) => {
-        geometry.scale(0.0024, 0.0024, 0.0024);
-        geometry.computeVertexNormals();
-
-        const material = new THREE.MeshLambertMaterial();
-        const mesh = new THREE.Mesh(geometry, material);
-        mesh.rotation.y = -Math.PI / 2;
-        mesh.position.y = 0.8;
-        mesh.castShadow = true;
-        mesh.receiveShadow = true;
-        this.scene.add(mesh);
-      });
-
-      window.addEventListener('resize', this.onWindowResize.bind(this));
-
+      this.setupRenderer(canvas);
+      this.setupScene();
+      this.setupCamera(canvas);
+      this.setupControls();
+      this.setupLighting();
+      this.loadModels();
+      this.addEventListeners();
       this.animate();
     }
+  }
+
+  private setupRenderer(canvas: ElementRef<HTMLCanvasElement>): void {
+    this.renderer = new THREE.WebGLRenderer({ canvas: canvas.nativeElement, antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    this.renderer.toneMappingExposure = 1;
+  }
+
+  private setupScene(): void {
+    this.scene = new THREE.Scene();
+  }
+
+  private setupCamera(canvas: ElementRef<HTMLCanvasElement>): void {
+    this.camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 100);
+    this.camera.position.set(7, 4, 1);
+  }
+
+  private setupControls(): void {
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableZoom = false; // Deshabilitar zoom
+    this.controls.minDistance = 2;
+    this.controls.maxDistance = 10;
+    this.controls.maxPolarAngle = Math.PI / 2;
+    this.controls.target.set(0, 1, 0);
+    this.controls.update();
+  }
+
+  private setupLighting(): void {
+    const ambient = new THREE.HemisphereLight(0xffffff, 0x8d8d8d, 0.15);
+    this.scene.add(ambient);
+
+    const loader = new THREE.TextureLoader().setPath('textures/');
+    const textures = {
+      'disturb.jpg': loader.load('disturb.jpg'),
+      'colors.png': loader.load('colors.png'),
+      'uv_grid_opengl.jpg': loader.load('uv_grid_opengl.jpg')
+    };
+
+    this.spotLight = new THREE.SpotLight(0xffffff, 100);
+    this.spotLight.position.set(2.5, 5, 2.5);
+    this.spotLight.angle = Math.PI / 6;
+    this.spotLight.penumbra = 1;
+    this.spotLight.decay = 2;
+    this.spotLight.distance = 0;
+    this.spotLight.map = textures['disturb.jpg'];
+    this.spotLight.castShadow = true;
+    this.spotLight.shadow.mapSize.width = 1024;
+    this.spotLight.shadow.mapSize.height = 1024;
+    this.spotLight.shadow.camera.near = 1;
+    this.spotLight.shadow.camera.far = 10;
+    this.spotLight.shadow.focus = 1;
+    this.scene.add(this.spotLight);
+
+    this.lightHelper = new THREE.SpotLightHelper(this.spotLight);
+    //this.scene.add(this.lightHelper);
+
+    const planeGeometry = new THREE.PlaneGeometry(200, 200);
+    const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xbabcbc });
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.position.set(0, -1, 0);
+    plane.rotation.x = -Math.PI / 2;
+    plane.receiveShadow = true;
+    this.scene.add(plane);
+  }
+
+  private loadModels(): void {
+    new PLYLoader().load('models/ply/binary/Lucy100k.ply', (geometry) => {
+      geometry.scale(0.0024, 0.0024, 0.0024);
+      geometry.computeVertexNormals();
+
+      const material = new THREE.MeshLambertMaterial();
+      const mesh = new THREE.Mesh(geometry, material);
+      mesh.rotation.y = -Math.PI / 2;
+      mesh.position.y = 0.8;
+      mesh.castShadow = true;
+      mesh.receiveShadow = true;
+      this.scene.add(mesh);
+    });
+  }
+
+  private addEventListeners(): void {
+    window.addEventListener('resize', this.onWindowResize.bind(this));
   }
 
   private animate(): void {
